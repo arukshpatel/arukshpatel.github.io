19024fe032125bf3e642e9b022af9b2ce2665264
Aruksh Patel <arukshpatel98@gmail.com>
2020-04-03 17:04:41 -0500

    testing

--- Script/script.js
+++ Script/script.js
@@ -3,6 +3,7 @@ function git() {
         var responseObj = JSON.parse(this.responseText);
         var length = responseObj.length;
         var stringOut = "";
+        var totalCommits;
 
         for (var i = 0; i < length; i++) {
             stringOut += "<tr>\n\t<th scrope='row'>" + (i + 1) + "</th>\n";
@@ -36,3 +37,17 @@ function PageUp() {
     window.scrollTo(0, 0);
     scrolldelay = setInterval('pageScroll()', 100);
 };
+
+function map() {
+    var map = L.map('map').setView([51.505, -0.09], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.marker([51.5, -0.09]).addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
+
+    console.log(map);
+}
